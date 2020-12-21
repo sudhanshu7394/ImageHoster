d1ded82949fcb59eb3e82329fcf8f31ce010a942
sudhanshu7394 <sudhanshu.yadav31@gmail.com>
2020-12-21 15:57:20 +0530

    Applied changes in controller to increse strength of password

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.regex.Pattern;
 
 
 @Controller
@@ -40,9 +41,21 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user, Model model) {
+        //Initialise the error message for password
+        String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+
+        model.addAttribute("User", user);
+        //adding error string to model object
+        model.addAttribute("passwordTypeError", error);
+
+        if(Pattern.matches("^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{3,}$", user.getPassword())) {
+            userService.registerUser(user);
+            return "users/login";
+        } else {
+            return "users/registration";
+        }
+
     }
 
     //This controller method is called when the request pattern is of type 'users/login'
